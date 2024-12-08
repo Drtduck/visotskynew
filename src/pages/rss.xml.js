b676dc0a337637d47540db478018edf9a1e19c2f
Drtduck <111480989+Drtduck@users.noreply.github.com>
2024-12-08 14:06:49 -0300

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,38 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import { formatRssDate, generateEnclosure, generateItemDescription } from '../utils/rssUtils';
 
 export async function GET(context) {
   const blog = await getCollection('blog');
+  
   return rss({
     title: 'Андрей Высоцкий | Блог',
     description: 'Психолог для собственников бизнеса и топ-менеджеров. Помогаю преодолевать барьеры для роста.',
     site: context.site,
+    xmlns: {
+      yandex: "http://news.yandex.ru",
+      media: "http://search.yahoo.com/mrss/"
+    },
+    customData: `
+      <language>ru</language>
+      <yandex:logo>https://visotski.psy/images/profile.jpg</yandex:logo>
+      <yandex:logo type="square">https://visotski.psy/images/profile.jpg</yandex:logo>
+      <generator>Astro v4.15.3</generator>
+    `,
     items: blog.map((post) => ({
       title: post.data.title,
+      description: generateItemDescription(post),
       pubDate: post.data.date,
-      description: post.data.description,
       link: `/blog/${post.slug}/`,
+      enclosure: generateEnclosure(post),
+      customData: post.data.image ? `
+        <media:content 
+          url="${post.data.image}"
+          type="image/jpeg"
+          medium="image"
+        />
+        <yandex:full-text>${post.data.description}</yandex:full-text>
+      ` : ''
     })),
-    customData: `<language>ru-ru</language>`,
   });
 }
